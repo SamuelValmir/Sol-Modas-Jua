48060ae479d65ae2414e65ab15f165ef5921ffeb
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2022-06-27 18:38:57 -0300

    Update: Clothes and Gender dropdown

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,34 @@
-window.addEventListener("resize", ()=>{
+window.addEventListener("resize", () => {
     resizeBackground();
 });
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     resizeBackground();
 });
 
-function resizeBackground(){
+function resizeBackground() {
     const background = document.querySelector(".background");
     const footer = document.querySelector("footer");
 
     background.style.height = footer.offsetTop + "px";
 };
+
+let dropdown = document.querySelector(".dropdown-toggle");
+let dropdownItems = document.querySelectorAll(".dropdown-menu .dropdown-item");
+
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener("click", (item) => {
+        const dropdownItem = item.target;
+
+
+        // It deactivates all dropdown items 
+        dropdownItems.forEach(dropdownItem => {
+            dropdownItem.classList.remove("active");
+        });
+
+        dropdownItem.classList.add("active");
+        dropdown.innerHTML = dropdownItem.innerHTML;
+    })
+});
+
+console.log(dropdownItems)
