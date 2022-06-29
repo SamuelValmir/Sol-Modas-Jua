ef7450694cb29865847b5c61c290b535cd17171a
Samuel Valmir <53239510+SamuelValmir@users.noreply.github.com>
2022-06-29 19:14:10 -0300

    Feature: Validate form email

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+// const transporter = require("nodemailer").createTransport(
+//     {
+//         host: "smtp.gmail.com",
+//         service: "gmail",
+//         port: 465,
+//         secure: true,
+//         auth:{
+//             user: "samuelvalmir@gmail.com",
+//             pass: "Esmaelbananinha21141141!@"
+//         }
+//     }
+// );
 
 const app = express();
 const port = 8888;
 
-app.use(cors({origin: `http://192.168.100.24:${port}`}))
+app.use(cors({ origin: `http://192.168.100.24:${port}` }))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.post("/api/sendEmail", (req, res)=>{
+app.post("/api/sendEmail", (req, res) => {
     console.log(req.query);
     res.end();
 });
@@ -16,3 +28,14 @@ app.post("/api/sendEmail", (req, res)=>{
 app.listen(port, () => {
     console.log(`Server Running On Port ${port}`);
 });
+
+// const mailOptions = {
+//     from: "samuelvalmir@gmail.com",
+//     to: "samuelvalmir@gmail.com",
+//     subject: "Assssssunto",
+//     html: "conteudo é esse",
+// }
+
+// transporter.sendMail(mailOptions, (error, info)=>{
+//     if(error) console.warn(error);
+// })
